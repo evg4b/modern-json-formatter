780575c941bb666b528b6d9b33b7803ec4fa3ca9
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-05-12 17:36:49 -0300

    Updated test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,21 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  cache: true,
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/.yarn/',
+  ],
+  coverageProvider: 'v8',
+  coverageReporters: ['lcov', 'text'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/*.d.ts',
+  ],
+  verbose: true,
   transform: {
     '^.+\\.[tj]sx?$': [
       'ts-jest',

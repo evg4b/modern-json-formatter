8e05f2d0155bf60b6c372247897c9f6c5dfc133f
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-05-12 14:49:39 -0300

    Refactored tests

--- src/parser/parser.test.ts
+++ src/parser/parser.test.ts
@@ -3,11 +3,36 @@ import fs from 'fs/promises';
 import '../../parser/wasm_exec.js';
 
 describe('parser wasm', () => {
-  const go = new Go();
+  let go: Go;
+
+  interface TestCase {
+    name: string;
+    input: string;
+    expected: ParsedJSON;
+  }
+
+  const tNull = (): NullNode => ({ type: 'null' });
+  const tBool = (value: boolean): BooleanNode => ({ type: 'bool', value: value });
+  const tString = (value: string): StringNode => ({ type: 'string', value: value });
+  const tNumber = (value: string): NumberNode => ({ type: 'number', value: value });
+  const tArray = (items: ParsedJSON[]): ArrayNode => ({ type: 'array', items });
+  const tObject = (properties: PropertyNode[]): ObjectNode => ({ type: 'object', properties });
+  const tProperty = (key: string, value: ParsedJSON): PropertyNode => ({ key, value: value });
+  const tResponse = (value: ParsedJSON): SuccessParserResponse => ({ type: 'response', value: value });
+
+  const runTestCases = (testCases: TestCase[]) => {
+    testCases.forEach(({ name, input, expected }) => {
+      it(name, () => {
+        const actual = parseJSON(input);
+        expect(actual).toEqual(tResponse(expected));
+      });
+    });
+  };
 
   beforeAll(async () => {
     const wasmBuffer = await fs.readFile('parser/parser.wasm');
     const wasm = await WebAssembly.instantiate(wasmBuffer, go.importObject);
+    go = new Go();
     go.run(wasm.instance);
   });
 
@@ -28,160 +53,91 @@ describe('parser wasm', () => {
     });
 
     describe('boolean', () => {
-      it('true', () => {
-        const actual = parseJSON('true');
-        expect(actual).toEqual(
-          tResponse(tBool(true)),
-        );
-      });
-
-      it('false', () => {
-        const actual = parseJSON('false');
-        expect(actual).toEqual(tResponse(tBool(false)));
-      });
+      runTestCases([
+        { name: 'true', input: 'true', expected: tBool(true) },
+        { name: 'false', input: 'false', expected: tBool(false) },
+      ]);
     });
 
     describe('number', () => {
       describe('integer', () => {
-        it('positive', () => {
-          const actual = parseJSON('42');
-          expect(actual).toEqual(
-            tResponse(tNumber('42')),
-          );
-        });
-
-        it('negative', () => {
-          const actual = parseJSON('-42');
-          expect(actual).toEqual(
-            tResponse(tNumber('-42')),
-          );
-        });
+        runTestCases([
+          { name: 'positive', input: '42', expected: tNumber('42') },
+          { name: 'negative', input: '-42', expected: tNumber('-42') },
+        ]);
       });
 
       describe('float', () => {
-        it('positive', () => {
-          const actual = parseJSON('42.42');
-          expect(actual).toEqual(
-            tResponse(tNumber('42.42')),
-          );
-        });
-
-        it('negative', () => {
-          const actual = parseJSON('-42.42');
-          expect(actual).toEqual(
-            tResponse(tNumber('-42.42')),
-          );
-        });
-
-        it('maximum js value + 1', () => {
-          const actual = parseJSON('23064690611454417');
-          expect(actual).toEqual(
-            tResponse(tNumber('23064690611454417')),
-          );
-        });
+        runTestCases([
+          { name: 'positive', input: '42.42', expected: tNumber('42.42') },
+          { name: 'negative', input: '-42.42', expected: tNumber('-42.42') },
+          { name: 'maximum js value + 1', input: '23064690611454417', expected: tNumber('23064690611454417') },
+        ]);
 
         describe('integers', () => {
-          const cases = [
-            { name: 'MaxInt8', value: '127' },
-            { name: 'MinInt8', value: '-128' },
-            { name: 'MaxInt16', value: '32767' },
-            { name: 'MinInt16', value: '-32768' },
-            { name: 'MaxInt32', value: '2147483647' },
-            { name: 'MinInt32', value: '-2147483648' },
-            { name: 'MaxInt64', value: '9223372036854775807' },
-            { name: 'MinInt64', value: '-9223372036854775808' },
-          ];
-
-          cases.forEach(({ name, value }) => {
-            it(name, () => {
-              const actual = parseJSON(value);
-              expect(actual).toEqual(
-                tResponse(tNumber(value)),
-              );
-            });
-          });
+          runTestCases([
+            { name: 'MaxInt8', input: '127', expected: tNumber('127') },
+            { name: 'MinInt8', input: '-128', expected: tNumber('-128') },
+            { name: 'MaxInt16', input: '32767', expected: tNumber('32767') },
+            { name: 'MinInt16', input: '-32768', expected: tNumber('-32768') },
+            { name: 'MaxInt32', input: '2147483647', expected: tNumber('2147483647') },
+            { name: 'MinInt32', input: '-2147483648', expected: tNumber('-2147483648') },
+            { name: 'MaxInt64', input: '9223372036854775807', expected: tNumber('9223372036854775807') },
+            { name: 'MinInt64', input: '-9223372036854775808', expected: tNumber('-9223372036854775808') },
+          ]);
         });
 
         describe('unsigned integers', () => {
-          const cases = [
-            { name: 'MaxUint8', value: '255' },
-            { name: 'MaxUint16', value: '65535' },
-            { name: 'MaxUint32', value: '4294967295' },
-            { name: 'MaxUint64', value: '18446744073709551615' },
-          ];
-
-          cases.forEach(({ name, value }) => {
-            it(name, () => {
-              const actual = parseJSON(value);
-              expect(actual).toEqual(
-                tResponse(tNumber(value)),
-              );
-            });
-          });
+          runTestCases([
+            { name: 'MaxUint8', input: '255', expected: tNumber('255') },
+            { name: 'MaxUint16', input: '65535', expected: tNumber('65535') },
+            { name: 'MaxUint32', input: '4294967295', expected: tNumber('4294967295') },
+            { name: 'MaxUint64', input: '18446744073709551615', expected: tNumber('18446744073709551615') },
+          ]);
         });
       });
     });
 
     describe('string', () => {
-      it('empty', () => {
-        const actual = parseJSON('""');
-        expect(actual).toEqual(tResponse(tString('')));
-      });
-
-      it('non-empty', () => {
-        const actual = parseJSON('"hello"');
-        expect(actual).toEqual(tResponse(tString('hello')));
-      });
-
-      it('with escaped quotes', () => {
-        const actual = parseJSON('"hello \\"world\\""');
-        expect(actual).toEqual(tResponse(tString('hello "world"')));
-      });
-
-      it('with escaped backslashes', () => {
-        const actual = parseJSON('"hello \\\\world"');
-        expect(actual).toEqual(tResponse(tString('hello \\world')));
-      });
+      runTestCases([
+        { name: 'empty', input: '""', expected: tString('') },
+        { name: 'non-empty', input: '"hello"', expected: tString('hello') },
+        { name: 'with escaped quotes', input: '"hello \\"world\\""', expected: tString('hello "world"') },
+        { name: 'with escaped backslashes', input: '"hello \\\\world"', expected: tString('hello \\world') },
+      ]);
     });
   });
 
-  test('should parse JSON', () => {
-    const actual = parseJSON('{"a": 1}');
-    expect(actual).toEqual(tResponse(
-      tObject([
-        tProperty('a', tNumber('1')),
-      ]),
-    ));
-  });
-
-  test('should parse JSON with nested objects', () => {
-    const actual = parseJSON('{"a": {"b": 2}}');
-    expect(actual).toEqual(tResponse(
-      tObject([
-        tProperty('a', tObject([
-          tProperty('b', tNumber('2')),
-        ])),
-      ]),
-    ));
-  });
-
-  test('should parse JSON with arrays', () => {
-    const actual = parseJSON('[1, 2, 3]');
-    expect(actual).toEqual(tResponse(
-      tArray([
-        tNumber('1'),
-        tNumber('2'),
-        tNumber('3'),
-      ]),
-    ));
-  });
+  runTestCases([
+    { name: 'should parse empty array', input: '[]', expected: tArray([]) },
+    { name: 'should parse empty object', input: '{}', expected: tObject([]) },
+    {
+      name: 'should parse JSON',
+      input: '{"a": 1}',
+      expected:
+        tObject([
+          tProperty('a', tNumber('1')),
+        ]),
+    },
+    {
+      name: 'should parse JSON with nested objects',
+      input: '{"a": {"b": 2}}',
+      expected:
+        tObject([
+          tProperty('a', tObject([
+            tProperty('b', tNumber('2')),
+          ])),
+        ]),
+    },
+    {
+      name: 'should parse JSON with arrays',
+      input: '[1, 2, 3]',
+      expected:
+        tArray([
+          tNumber('1'),
+          tNumber('2'),
+          tNumber('3'),
+        ]),
+    },
+  ]);
 });
-
-const tNull = (): NullNode => ({ type: 'null' });
-const tBool = (value: boolean): BooleanNode => ({ type: 'bool', value });
-const tString = (value: string): StringNode => ({ type: 'string', value });
-const tNumber = (value: string): NumberNode => ({ type: 'number', value });
-const tArray = (items: ParsedJSON[]): ArrayNode => ({ type: 'array', items });
-const tObject = (properties: PropertyNode[]): ObjectNode => ({ type: 'object', properties });
-const tProperty = (key: string, value: ParsedJSON): PropertyNode => ({ key, value });
-const tResponse = (value: ParsedJSON): SuccessParserResponse => ({ type: 'response', value });

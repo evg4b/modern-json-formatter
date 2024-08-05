1bc9cdd4ac2250692ee908cfc00ea97d602688af
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-08-05 15:33:40 -0300

    Updated wasm

--- packages/pkg/jq/normalise.go
+++ packages/pkg/jq/normalise.go
@@ -3,7 +3,6 @@ package jq
 import (
 	"math/big"
 	"packages/pkg/tokens"
-	"strconv"
 )
 
 func normalise(value any) any {
@@ -11,33 +10,19 @@ func normalise(value any) any {
 	case string:
 		return tokens.StringNode(value.(string))
 	case float64:
-		return tokens.NumberNode(
-			strconv.FormatFloat(value.(float64), 'f', -1, 64),
-		)
+		return tokens.NumberNode(formatFloat(value))
 	case bool:
 		return tokens.BoolNode(value.(bool))
 	case int:
-		return tokens.NumberNode(strconv.Itoa(value.(int)))
+		return tokens.NumberNode(formatInt(value))
 	case nil:
 		return tokens.NullNode()
 	case *big.Int:
 		return tokens.NumberNode(value.(*big.Int).String())
 	case []any:
-		items := value.([]any)
-		mapped := make([]any, len(items))
-		for i, item := range items {
-			mapped[i] = normalise(item)
-		}
-
-		return tokens.ArrayNode(mapped)
+		return tokens.ArrayNode(arrayItems(value))
 	case map[string]any:
-		properties := value.(map[string]any)
-		mapped := make([]any, 0, len(properties))
-		for key, value := range properties {
-			mapped = append(mapped, tokens.PropertyNode(key, normalise(value)))
-		}
-
-		return tokens.ObjectNode(mapped)
+		return tokens.ObjectNode(objectProperties(value))
 	default:
 		panic("unexpected type")
 	}

7c3fc2d969a114745e2b64e5be2893916a52890c
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-09-02 09:42:17 -0300

    Set headless

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -6,7 +6,7 @@ export const test = base.extend<{ testInfo: TestInfo, expectRequest: (url: strin
   context: async ({}, use) => {
     const pathToExtension = path.join(__dirname, '../dist');
     const context = await chromium.launchPersistentContext('', {
-      headless: false,
+      headless: true,
       args: [
         `--disable-extensions-except=${ pathToExtension }`,
         `--load-extension=${ pathToExtension }`,

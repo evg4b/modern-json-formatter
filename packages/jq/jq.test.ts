9e6cb142783b20c4f2584fb100ee383cbc94c4ae
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-11-02 15:00:08 -0300

    Added base test cases

--- packages/jq/jq.test.ts
+++ packages/jq/jq.test.ts
@@ -2,6 +2,7 @@ import '@testing/browser.mock';
 import { tNumber } from '@testing/json';
 import { readFileSync } from 'fs';
 import { jq } from './index';
+import testCases from './jq.test-cases.json';
 
 jest.mock('../shared/wasm_helpers.ts', () => ({
   loadWasm: (_: string, imports: WebAssembly.Imports) => {
@@ -16,4 +17,43 @@ describe('jq', () => {
 
     expect(data).toEqual(tNumber(`123`));
   });
+
+  describe.each(testCases)('$title', ({ sections }) => {
+    describe.each(sections as any[])('$title', ({ examples }) => {
+      // @ts-ignore
+      test.each(examples)('%p', async (test: any) => {
+        const data = await jq(test.input, test.query);
+
+        expect(data).toEqual(test.output)
+      });
+    });
+  });
+});
+
+
+const sections = document.querySelectorAll('main section');
+Array.from(sections).flatMap((section) => {
+  const header = section.querySelector('h2');
+  if (!header) {
+    return [];
+  }
+
+  return {
+    title: header.innerText,
+    sections: Array.from(section.querySelectorAll('section')).flatMap(subSection => {
+      const header = subSection.querySelector('h3');
+      if (!header) {
+        return [];
+      }
+
+      return {
+        title: header.innerText,
+        items: Array.from(subSection.querySelectorAll('table'))
+          .map(example => {
+            return Array.from(example.querySelectorAll('tr td'))
+              .map(p => p.textContent);
+          }),
+      };
+    }),
+  };
 });

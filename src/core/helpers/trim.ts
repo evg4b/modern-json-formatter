1c2dc9b36fe0b83708c15c37b9956b41d78e4de1
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-11-10 17:06:47 -0400

    Fix

--- src/core/helpers/trim.ts
+++ src/core/helpers/trim.ts
@@ -1,4 +1,4 @@
-export const trim = (value: string, chars: string = ' \t\n\r') => {
+export const trim = (value: string, chars: string) => {
   const pattern = new RegExp(`^[${chars}]+|[${chars}]+$`, 'g');
   return value.replace(pattern, '');
 };

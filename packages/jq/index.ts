0c0e84b90e46925499bc23a77583f5be4189583e
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-11-10 20:40:11 -0400

    Refractored packages

--- packages/jq/index.ts
+++ packages/jq/index.ts
@@ -4,11 +4,11 @@ import { importWasm } from '../shared';
 let go = new Go();
 
 export const jq = async (data: string, query: string): Promise<TokenizerResponse> => {
-  if (!('___jq' in window) || go.exited) {
+  if (!('___jq' in globalThis) || go.exited) {
     go = new Go();
     await importWasm(go, 'jq.wasm');
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return window.___jq(data, query);
+  return globalThis.___jq(data, query);
 };

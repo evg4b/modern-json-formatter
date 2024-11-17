3fa170c694550a9d3289f196ef1b2f1c371f7413
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-11-16 20:57:30 -0400

    Added tests for getNodeWithCode

--- src/content-script/json-detector/get-node-with-code.ts
+++ src/content-script/json-detector/get-node-with-code.ts
@@ -1,16 +1,19 @@
 export const getNodeWithCode = (list: NodeListOf<ChildNode>): HTMLPreElement | HTMLDivElement | null => {
   const items = Array.from(list);
-  const pre = items.find(node => node.nodeName === 'PRE') as HTMLPreElement | null;
+  const pre = items.find(node => node.nodeName === 'PRE') ?? null;
 
   if (!pre && navigator.userAgent.includes('Edg')) {
-    return Array.from(list).find((node) => {
-      if (!(node instanceof HTMLDivElement)) {
-        return false;
-      }
-      const attributes = node.getAttributeNames();
-      return attributes.length === 1 && attributes[0] === 'hidden';
-    }) as HTMLDivElement | null;
+    const div = Array.from(list)
+      .find((node) => {
+        if (!(node instanceof HTMLDivElement)) {
+          return false;
+        }
+        const attributes = node.getAttributeNames();
+        return attributes.length === 1 && attributes[0] === 'hidden';
+      }) ?? null;
+
+    return div as HTMLDivElement | null;
   }
 
-  return pre;
+  return pre as HTMLPreElement | null;
 };

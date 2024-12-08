239ca6351f76187f0df03aef2296d52222eff060
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-12-08 11:51:06 -0400

    Added changing histiry

---
+++ src/content-script/ui/query-input/query-input.helpres.spec.ts
@@ -0,0 +1,59 @@
+import { describe, expect, test } from '@jest/globals';
+import { isRedoEvent, isSubmitEvent, isUndoEvent, isWrapEvent } from './query-input.helpres';
+
+interface TestCase { event: KeyboardEventInit; expected: boolean }
+
+describe('isUndoEvent', () => {
+  const cases: TestCase[] = [
+    { event: { key: 'z', metaKey: true, shiftKey: false }, expected: true },
+    { event: { key: 'z', ctrlKey: true, shiftKey: false }, expected: true },
+    { event: { key: 'a', metaKey: true, shiftKey: false }, expected: false },
+    { event: { key: 'a', ctrlKey: true, shiftKey: false }, expected: false },
+    { event: { key: 'z', metaKey: false, shiftKey: false }, expected: false },
+    { event: { key: 'z', ctrlKey: false, shiftKey: false }, expected: false },
+  ];
+
+  test.each(cases)('should return $expected for event $event', ({ event, expected }) => {
+    expect(isUndoEvent(new KeyboardEvent('keydown', event))).toBe(expected);
+  });
+});
+
+describe('isRedoEvent', () => {
+  const cases: TestCase[] = [
+    { event: { key: 'z', metaKey: true, shiftKey: true }, expected: true },
+    { event: { key: 'z', ctrlKey: true, shiftKey: true }, expected: true },
+    { event: { key: 'z', metaKey: true, shiftKey: false }, expected: false },
+    { event: { key: 'a', metaKey: true, shiftKey: true }, expected: false },
+  ];
+
+  test.each(cases)('should return $expected for event $event', ({ event, expected }) => {
+    expect(isRedoEvent(new KeyboardEvent('keydown', event))).toBe(expected);
+  });
+});
+
+describe('isWrapEvent', () => {
+  const brackets = { '(': ')', '[': ']', '{': '}' };
+
+  const cases: TestCase[] = [
+    { event: { key: '(' }, expected: true },
+    { event: { key: '[' }, expected: true },
+    { event: { key: 'a' }, expected: false },
+    { event: { key: ')' }, expected: false },
+  ];
+
+  test.each(cases)('should return $expected for event $event', ({ event, expected }) => {
+    expect(isWrapEvent(new KeyboardEvent('keydown', event), brackets)).toBe(expected);
+  });
+});
+
+describe('isSubmitEvent', () => {
+  const cases: TestCase[] = [
+    { event: { key: 'Enter' }, expected: true },
+    { event: { key: 'a' }, expected: false },
+    { event: { key: 'Tab' }, expected: false },
+  ];
+
+  test.each(cases)('should return $expected for event $event', ({ event, expected }) => {
+    expect(isSubmitEvent(new KeyboardEvent('keydown', event))).toBe(expected);
+  });
+});
--- src/content-script/ui/query-input/query-input.ts
+++ src/content-script/ui/query-input/query-input.ts
@@ -2,33 +2,45 @@ import { getURL } from '@core/browser';
 import { createElement, CustomElement, StyledComponentElement } from '@core/dom';
 import { isNotNil } from '../../helpres';
 import { InfoButtonElement } from '../info-button';
+import { HistoryManager } from './history-manager';
+import { isRedoEvent, isSubmitEvent, isUndoEvent, isWrapEvent } from './query-input.helpres';
 import queryInputStyles from './query-input.module.scss';
 
-const endKeys: Record<string, string> = {
+const brackets: Record<string, string> = {
   '[': ']',
   '(': ')',
   '{': '}',
-  '\'': '\'',
+  "'": "'",
   '"': '"',
   '`': '`',
 };
+
+interface HistoryItem {
+  query: string;
+  start: number | null;
+  end: number | null;
+}
 
 @CustomElement('query-input')
 export class QueryInputElement extends StyledComponentElement {
   private readonly input = this.createInput();
   private readonly errorMessage = this.createErrorMessage();
+  private readonly wrapper = createElement({
+    element: 'div',
+    class: 'input-wrapper',
+    children: [this.input, this.errorMessage],
+  });
+
   private readonly infoIcons = new InfoButtonElement(getURL('faq.html'));
 
   private onSubmitCallback: ((s: string) => unknown) | null = null;
+  private history = new HistoryManager<HistoryItem>();
 
   constructor() {
     super(queryInputStyles);
-    const wrapper = createElement({
-      element: 'div',
-      class: 'input-wrapper',
-      children: [this.input, this.errorMessage],
-    });
-    this.shadow.append(this.infoIcons, wrapper);
+    this.shadow.append(this.infoIcons, this.wrapper);
+    this.setupEventHandlers(this.input);
+    this.saveState();
   }
 
   public setErrorMessage(errorMessage: string | null): void {
@@ -65,22 +77,6 @@ export class QueryInputElement extends StyledComponentElement {
     const input = createElement({ element: 'input' });
     input.type = 'text';
     input.placeholder = 'Input jq query...';
-    input.addEventListener('keydown', event => {
-      this.setErrorMessage(null);
-      if (event.key === 'Enter') {
-        this.onSubmitCallback?.(input.value);
-      }
-      if (endKeys[event.key]) {
-        const start = input.selectionStart ?? 0;
-        const end = input.selectionEnd ?? 0;
-        if (isNotNil(start) && isNotNil(end) && start !== end) {
-          event.preventDefault();
-          const selectedText = input.value.substring(start, end);
-          input.setRangeText(`${ event.key }${ selectedText }${ endKeys[event.key] }`);
-          input.setSelectionRange(start + 1, end + 1);
-        }
-      }
-    });
 
     return input;
   }
@@ -89,5 +85,68 @@ export class QueryInputElement extends StyledComponentElement {
     const errorMessage = document.createElement('span');
     errorMessage.classList.add('error-message', 'hidden');
     return errorMessage;
+  }
+
+  private setupEventHandlers(input: HTMLInputElement) {
+    input.addEventListener('keydown', event => {
+      this.setErrorMessage(null);
+      if (isSubmitEvent(event)) {
+        this.onSubmitEvent(event);
+      }
+      if (isWrapEvent(event, brackets)) {
+        this.onWrapEvent(event);
+      }
+      if (isUndoEvent(event)) {
+        this.onUndoEvent(event);
+      }
+      if (isRedoEvent(event)) {
+        this.onRedoEvent(event);
+      }
+    });
+
+    input.addEventListener('input', this.saveState.bind(this));
+  }
+
+  private onSubmitEvent(_: KeyboardEvent) {
+    this.onSubmitCallback?.(this.input.value);
+  }
+
+  private onWrapEvent(event: KeyboardEvent) {
+    const start = this.input.selectionStart ?? 0;
+    const end = this.input.selectionEnd ?? 0;
+    if (isNotNil(start) && isNotNil(end) && start !== end) {
+      event.preventDefault();
+      this.saveState();
+      const selectedText = this.input.value.substring(start, end);
+      this.input.setRangeText(`${event.key}${selectedText}${brackets[event.key]}`);
+      this.input.setSelectionRange(start + 1, end + 1);
+    }
+  }
+
+  private onUndoEvent(event: KeyboardEvent) {
+    event.preventDefault();
+    const previousState = this.history.undo();
+    this.restoreState(previousState);
+  }
+
+  private onRedoEvent(event: KeyboardEvent) {
+    event.preventDefault();
+    const nextState = this.history.redo();
+    this.restoreState(nextState);
+  }
+
+  private saveState() {
+    this.history.save({
+      query: this.input.value,
+      start: this.input.selectionStart,
+      end: this.input.selectionEnd,
+    });
+  }
+
+  private restoreState(state: HistoryItem | null) {
+    if (state) {
+      this.input.value = state.query;
+      this.input.setSelectionRange(state.start, state.end);
+    }
   }
 }

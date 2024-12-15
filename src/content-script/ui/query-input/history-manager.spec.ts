e4ae0f5cf06ebac4554ab1450bf285680ecb4b52
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-12-15 10:49:12 -0400

    Refactored history manager

---
+++ src/content-script/ui/query-input/history-manager.spec.ts
@@ -0,0 +1,48 @@
+import { describe, expect, it } from "@jest/globals";
+import { HistoryManager } from './history-manager';
+
+describe('HistoryManager', () => {
+  describe('undo', () => {
+    it('should undo the last change and return the previous state', () => {
+      const historyManager = new HistoryManager<number>();
+      historyManager.save(1);
+      historyManager.save(2);
+      expect(historyManager.undo()).toBe(1);
+    });
+
+    it('should return null if there are no previous states', () => {
+      const historyManager = new HistoryManager<number>();
+      expect(historyManager.undo()).toBeNull();
+    });
+  });
+
+  describe('redo', () => {
+    it('should redo the last undone change and return the restored state', () => {
+      const historyManager = new HistoryManager<number>();
+      historyManager.save(1);
+      historyManager.save(2);
+      historyManager.undo();
+      expect(historyManager.redo()).toBe(2);
+    });
+
+    it('should return null if there are no future states', () => {
+      const historyManager = new HistoryManager<number>();
+      historyManager.save(1);
+      historyManager.undo();
+      expect(historyManager.redo()).toBeNull();
+    });
+  });
+
+  describe('save', () => {
+    it('should save a new item in the history and clear any undone changes', () => {
+      const historyManager = new HistoryManager<number>();
+      historyManager.save(1);
+      historyManager.save(2);
+      historyManager.save(3);
+      historyManager.undo();
+      historyManager.undo();
+      historyManager.save(10)
+      expect(historyManager.redo()).toEqual(null);
+    });
+  });
+});
--- src/content-script/ui/query-input/history-manager.ts
+++ src/content-script/ui/query-input/history-manager.ts
@@ -1,37 +1,53 @@
+/**
+ * A class responsible for managing the history of changes.
+ *
+ * @template T The type of items being managed.
+ */
 export class HistoryManager<T> {
   private history: T[] = [];
   private current: T | null = null;
   private future: T[] = [];
 
+  /**
+   * Undoes the last change, returning the previous state.
+   *
+   * @returns {T | null} The previous state of the item, or null if there are no previous states.
+   */
   public undo(): T | null {
-    if (!this.history.length) {
-      return null;
-    }
-
-    if (this.current) {
-      this.future.push(this.current);
-    }
-
-    return this.current = this.history.pop() ?? null;
+    return this.swap(this.history, this.future);
   }
 
+  /**
+   * Redoes the last undone change, returning the restored state.
+   *
+   * @returns {T | null} The restored state of the item, or null if there are no future states.
+   */
   public redo(): T | null {
-    if (!this.future.length) {
-      return null;
-    }
-
-    if (this.current) {
-      this.history.push(this.current);
-    }
-
-    return this.current = this.future.pop() ?? null;
+    return this.swap(this.future, this.history);
   }
 
+  /**
+   * Saves a new item in the history and clears any undone changes.
+   *
+   * @param {T} item The item to save.
+   */
   public save(item: T) {
     if (this.current) {
       this.history.push(this.current);
     }
     this.current = item;
     this.future = [];
+  }
+
+  private swap(a: T[], b: T[]): T | null {
+    if (!a.length) {
+      return null;
+    }
+
+    if (this.current) {
+      b.push(this.current);
+    }
+
+    return this.current = a.pop() ?? null;
   }
 }

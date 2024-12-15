a5d0c49fb121330e0b7a153fc5304cd7473a1e7e
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-12-15 18:53:43 -0400

    Updated packages

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,13 +3,13 @@ import { type Message } from '@core/background';
 import { initialize } from '@worker-core';
 import { handler } from './handler';
 
-// eslint-disable-next-line @typescript-eslint/no-deprecated
+ 
 chrome.runtime.onMessage.addListener((message: Message, _, sendResponse): unknown => {
   sendResponse(handler(message));
   return true;
 });
 
-// eslint-disable-next-line @typescript-eslint/no-deprecated
+ 
 chrome.runtime.onInstalled.addListener(() => {
   void initialize()
     .then(() => console.log('Worker initialized'))

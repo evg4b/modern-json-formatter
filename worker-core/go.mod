ec010df14b6f199356d3033e5f633e68cb02498c
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-12-15 17:10:03 -0400

    Added minimal build setup

---
+++ worker-core/go.mod
@@ -0,0 +1,3 @@
+module worker-core
+
+go 1.23.3
---
+++ worker-core/wasm_helpers.ts
@@ -0,0 +1,10 @@
+type Imports = WebAssembly.Imports;
+type WebAssemblyInstantiatedSource = WebAssembly.WebAssemblyInstantiatedSource;
+
+export const loadWasm = (url: string, imports: Imports): Promise<WebAssemblyInstantiatedSource> => {
+  return 'instantiateStreaming' in WebAssembly
+    ? WebAssembly.instantiateStreaming(fetch(url), imports)
+    : fetch(url)
+        .then(resp => resp.arrayBuffer())
+        .then(bytes => WebAssembly.instantiate(bytes, imports));
+};

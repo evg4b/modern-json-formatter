c52b52fa6396b7947a099a31fc3eac5f1ac38af4
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-02-17 13:10:13 -0400

    Updated packages

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,4 @@
 import '@webcomponents/custom-elements';
-import { TextDecoder, TextEncoder } from 'text-encoding';
-global.TextDecoder = TextDecoder;
-global.TextEncoder = TextEncoder;
+import { TextDecoder, TextEncoder } from 'node:util';
+Object.assign(global, { TextDecoder, TextEncoder });
 import './worker-core/wasm_exec.js';

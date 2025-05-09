c9a248a7c7d94fe40143608d6f322d4d697f55a6
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-05-09 18:59:10 -0300

    Added types

--- src/types.d.ts
+++ src/types.d.ts
@@ -8,6 +8,17 @@ declare module '*.html' {
   export default content;
 }
 
+interface MarkdownFile {
+  html: string;
+  raw: string;
+  filename: string;
+}
+
+declare module '*.md' {
+  declare const file: MarkdownFile;
+  export default file;
+}
+
 declare module '*.css?raw' {
   const content: string;
   export default content;

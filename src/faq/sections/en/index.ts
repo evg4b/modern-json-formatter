f481c200e3a51d7ee3fb2c8969aee19cc9b3e021
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-05-09 20:59:45 -0300

    Added Conditionals and Comparisons md

--- src/faq/sections/en/index.ts
+++ src/faq/sections/en/index.ts
@@ -2,10 +2,12 @@ import basicFilters from './basic-filters.md';
 import intro from './intro.md';
 import typesAndValues from './types-and-values.md';
 import builtinOperatorsAndFunctions from './builtin-operators-and-functions.md';
+import conditionalsAndComparisons from './conditionals-and-comparisons.md';
 
 export default {
   intro,
   basicFilters,
   typesAndValues,
   builtinOperatorsAndFunctions,
+  conditionalsAndComparisons,
 };

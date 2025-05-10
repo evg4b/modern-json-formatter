4ec8c7376f473859a032f77a41d7d9b2847fc645
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-05-09 21:53:59 -0300

    Foramat

---
+++ src/faq/sections/index.ts
@@ -0,0 +1,6 @@
+import en from "./en";
+import { FaqSection } from "./models";
+
+const lang: Record<"en", FaqSection> = { en };
+
+export default lang;
---
+++ src/faq/sections/models.ts
@@ -0,0 +1,11 @@
+export interface FaqSection {
+  intro: MarkdownFile;
+  basicFilters: MarkdownFile;
+  typesAndValues: MarkdownFile;
+  builtinOperatorsAndFunctions: MarkdownFile;
+  conditionalsAndComparisons: MarkdownFile;
+  regularExpressions: MarkdownFile;
+  advancedFeatures: MarkdownFile;
+  math: MarkdownFile;
+  assignment: MarkdownFile;
+}

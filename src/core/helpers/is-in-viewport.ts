2ba5ab3f527408edbe587750a894fbc8fd5a58a0
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-06-08 16:21:22 -0300

    Fixed issues

--- src/core/helpers/is-in-viewport.ts
+++ src/core/helpers/is-in-viewport.ts
@@ -3,7 +3,6 @@ export const isInViewport = (element: HTMLElement) => {
 
   return (
     rect.top >= 0 &&
-    rect.top >= 0 &&
     rect.left >= 0 &&
     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)

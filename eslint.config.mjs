c11832bbc86ffb76b2f0aa40a7704faffd288113
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-12-18 22:09:41 -0500

    Added lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import ts from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
   ...ts.configs.recommended,
+  stylistic.configs['recommended'],
+  {
+    rules: {
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+    },
+  },
   {
     ignores: [
       'dist/',
       'worker-core/wasm_exec.js',
       'tsup.config.mjs',
-    ]
+    ],
   },
 ];

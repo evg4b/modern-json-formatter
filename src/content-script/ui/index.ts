e394fbd2b86d09a6500add3a147f01e7dd928e81
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-12-19 00:19:45 -0500

    Delete obsolete code

--- src/content-script/ui/index.ts
+++ src/content-script/ui/index.ts
@@ -1,5 +1,4 @@
 export * from './info-button';
-export * from './query-input-old';
 export * from '@core/ui/floating-message';
 export * from './containers';
 export * from './toolbox';
--- src/core/hooks/use-async-effect.ts
+++
@@ -1,5 +0,0 @@
-import { type DependencyList, useEffect } from 'react';
-
-export const useAsyncEffect = (effect: () => Promise<unknown>, deps?: DependencyList) => {
-  useEffect(() => void effect(), deps);
-};

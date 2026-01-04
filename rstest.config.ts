54381f02b82c2710d74c62badaf71a3be780d48d
Evgeny Abramovich <evg.abramovitch@gmail.com>
2026-01-04 15:34:41 -0500

    Fixed lint issues

--- rstest.config.ts
+++ rstest.config.ts
@@ -20,9 +20,7 @@ export default defineConfig({
   ],
   coverage: {
     enabled: false,
-    exclude: [
-      'testing/**/*',
-    ],
+    exclude: ['testing/**/*'],
     include: [
       'src/**/*.ts',
       'worker-core/**/*.ts',

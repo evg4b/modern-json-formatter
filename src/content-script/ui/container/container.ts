94a5c074700bdd06c86a3ed115a432b14b97330b
Evgeny Abramovich <evg.abramovitch@gmail.com>
2026-03-25 00:46:51 -0400

    Added container

---
+++ src/content-script/ui/container/container.ts
@@ -0,0 +1,84 @@
+import { LitElement, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import styles from './container.scss?inline';
+import { createElement } from '@core/dom';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mjf-container': ContainerElement;
+  }
+}
+
+@customElement('mjf-container')
+export class ContainerElement extends LitElement {
+  public static override shadowRootOptions: ShadowRootInit = {
+    ...LitElement.shadowRootOptions,
+    delegatesFocus: true,
+    mode: 'closed',
+  };
+
+  public static override readonly styles = [unsafeCSS(styles)];
+
+  private readonly raw = document.createElement('div');
+  private readonly formated = document.createElement('div');
+  private readonly query = document.createElement('div');
+  private readonly loader = createElement({ element: 'div', class: 'loader' });
+
+  @property({ type: String })
+  public type: TabType = 'formatted';
+
+
+  public override render() {
+    return [this.loader, this.container];
+  }
+
+  public setRawContent(content: HTMLElement) {
+    this.raw.replaceChildren(content);
+  }
+
+  public setFormattedContent(content: HTMLElement) {
+    this.formated.replaceChildren(content);
+  }
+
+  public setQueryContent(content: HTMLElement) {
+    this.query.replaceChildren(content);
+  }
+
+  public setError(error: unknown) {
+    console.error(error);
+  }
+
+  public setLoadingState(isLoading: boolean) {
+    if (isLoading) {
+      this.setAttribute('loading', 'true');
+    } else {
+      this.removeAttribute('loading');
+    }
+  }
+
+  public message(header: string, content: string) {
+    const created = createElement({
+      element: 'mjf-floating-message',
+      attributes: {
+        type: 'info-message',
+        header: header,
+      },
+      content: content,
+    });
+
+    this.appendChild(created);
+  }
+
+  private get container(): HTMLElement {
+    switch (this.type) {
+      case 'raw':
+        return this.raw;
+      case 'formatted':
+        return this.formated;
+      case 'query':
+        return this.query;
+      default:
+        throw new Error('Unknown type');
+    }
+  }
+}
